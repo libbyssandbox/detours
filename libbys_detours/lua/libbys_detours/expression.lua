8f6ce7f7af1decb09b1b60b4de317f5bf4681ef2
Zach <147861361+1054009@users.noreply.github.com>
2024-06-25 22:36:25 -0400

    Test entity:pos detour

--- libbys_detours/lua/libbys_detours/expression.lua
+++ libbys_detours/lua/libbys_detours/expression.lua
@@ -2,7 +2,16 @@ AddCSLuaFile()
 
 local Detours = libbys:FindModule("Detours")
 
-Detours:CreateExpression2("pos(e:)", function(...)
-	print(...)
-	return Vector()
+Detours:CreateExpression2("pos(e:)", function(ctx, args)
+	local entity = args[1]
+	if not IsValid(entity) then
+		return _OriginalFunction_(ctx, args)
+	end
+
+	if entity:IsPlayer() and entity ~= ctx.player then
+		ctx:throw("You can't call :pos on other players!")
+		return Vector()
+	end
+
+	return _OriginalFunction_(ctx, args)
 end)
