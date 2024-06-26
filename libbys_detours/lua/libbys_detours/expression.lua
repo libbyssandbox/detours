a4b6b46f38ef18ffabd32cc7444a4b9c395cb133
Zach <147861361+1054009@users.noreply.github.com>
2024-06-26 09:50:48 -0400

    Add more OnlyOwnerPlayer detours
    Almost done

--- libbys_detours/lua/libbys_detours/expression.lua
+++ libbys_detours/lua/libbys_detours/expression.lua
@@ -26,3 +26,18 @@ end
 Entity_OnlyOwnerPlayer("pos(e:)")
 Entity_OnlyOwnerPlayer("toWorld(e:v)")
 Entity_OnlyOwnerPlayer("toLocal(e:v)")
+Entity_OnlyOwnerPlayer("toWorldAxis(e:v)")
+Entity_OnlyOwnerPlayer("toLocalAxis(e:v)")
+Entity_OnlyOwnerPlayer("bearing(e:v)")
+Entity_OnlyOwnerPlayer("elevation(e:v)")
+Entity_OnlyOwnerPlayer("heading(e:v)")
+Entity_OnlyOwnerPlayer("massCenter(e:)")
+-- setMass
+Entity_OnlyOwnerPlayer("boxCenterW(e:)")
+Entity_OnlyOwnerPlayer("aabbWorldMin(e:)")
+Entity_OnlyOwnerPlayer("aabbWorldMax(e:)")
+Entity_OnlyOwnerPlayer("aabbWorldSize(e:)")
+Entity_OnlyOwnerPlayer("attachmentPos(e:n)")
+Entity_OnlyOwnerPlayer("attachmentPos(e:s)")
+Entity_OnlyOwnerPlayer("nearestPoint(e:v)")
+-- noCollideAll
