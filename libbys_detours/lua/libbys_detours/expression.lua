3a5d5ca0eaa9b8b2048c81a16a48ee205dad95bb
Zach <147861361+1054009@users.noreply.github.com>
2024-06-26 09:09:57 -0400

    Return Vector(0,0,0)

--- libbys_detours/lua/libbys_detours/expression.lua
+++ libbys_detours/lua/libbys_detours/expression.lua
@@ -2,13 +2,14 @@ local Detours = libbys:FindModule("Detours")
 
 Detours:CreateExpression2("pos(e:)", function(ctx, args)
 	local entity = args[1]
+
 	if not IsValid(entity) then
 		return _OriginalFunction_(ctx, args)
 	end
 
 	if entity:IsPlayer() and entity ~= ctx.player then
 		ctx:throw("You can't call :pos on other players!")
-		return Vector()
+		return Vector(0, 0, 0)
 	end
 
 	return _OriginalFunction_(ctx, args)
