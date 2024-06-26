7f1a5b8ab01d50eef5a0194c2b22e152a654ba32
Zach <147861361+1054009@users.noreply.github.com>
2024-06-25 21:50:43 -0400

    Remove test detour

--- libbys_detours/lua/libbys_detours/generic.lua
+++ libbys_detours/lua/libbys_detours/generic.lua
@@ -2,7 +2,4 @@ AddCSLuaFile()
 
 local Detours = libbys:FindModule("Detours")
 
-Detours:CreateGeneric("util.SHA256", function(str)
-	print("hashing ", str)
-	return _OriginalFunction_(str)
-end)
+-- Nothing yet
